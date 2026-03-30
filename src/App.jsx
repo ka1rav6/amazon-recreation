eed6896d90d4202eb600041b093e79792fb63145
Kairav <kairavdutta@gmail.com>
2026-03-31 02:29:20 +0530

    added some images, created basic grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,67 @@
-import { useState } from 'react'
-
+// import { useState } from 'react'
+import imgData from "./data.json"
 import './App.css'
+const StarRating = ({ rating, total = 5 }) => {
+  return (
+    <div>
+      {[...Array(total)].map((_, i) => {
+        const starValue = i + 1;
 
-function App() {
-  
+        if (rating >= starValue) {
+          return <span key={i}>★</span>;
+        } else if (rating >= starValue - 0.5) {
+          return <span key={i}>⯪</span>;
+        } else {
+          return <span key={i}>☆</span>;
+        }
+      })}
+    </div>
+  );
+};
+function Image({ image, title, rating, num, price }) {
+  return (
+    <div className="image-card">
+      <div className="card-content">
+        <img src={image} className="image-display" />
+        <p className="title">{title}</p>
+        <div className="stars">
+          <StarRating rating={rating} />
+          {num}
+        </div>
+        <p className="price">
+          <strong>{'$' + price}</strong>
+        </p>
+      </div>
+      <button className="add-to-card-button">Add to Cart</button>
+    </div>
+  );
+}
+function AllImages(){
+  return(
+    <>
+    {
+      imgData.map(({image, title, rating, num, price}, index) =>{
+        return (
+          <Image 
+            key={index}
+            image={image} 
+            title={title} 
+            rating={rating} 
+            num={num}
+            price={price}
+          />
+        )
+      })
+    }
+    </>
+  )
+}
+function App(){
+  return(
+    <div className = "main-placeholder">
+    <AllImages />
+    </div>
+  )
 }
 
 export default App
